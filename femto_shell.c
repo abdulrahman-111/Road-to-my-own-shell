cda82c846130d3eb558b1173b5a24eff3e92da13
Abdulrahman Gomaa <154401115+abdulrahman-111@users.noreply.github.com>
2025-08-29 18:35:08 +0300

    Update femto_shell.c

--- femto_shell.c
+++ femto_shell.c
@@ -4,66 +4,60 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void my_echo(int argc , char ** argv){
- for (int x=1; x <argc; x++ ){
-        if (write(1,argv[x],strlen(argv[x]) )<0 ){
-        exit (1);
+void my_echo(int argc, char **argv) {
+    for (int x = 1; x < argc; x++) {
+        if (write(1, argv[x], strlen(argv[x])) < 0) {
+            exit(1);
         }
-        if (x < argc - 1)  write(1, " ", 1);
-  }
-  write(1,"\n",1);
+        if (x < argc - 1) write(1, " ", 1);
+    }
+    write(1, "\n", 1);
 }
 
-int tokenize(char input[] ,char **argv)
-{
-		int argc=0;
+int tokenize(char input[], char **argv) {
+    int argc = 0;
     char *token = strtok(input, " ");
-    while (token  && argc < 1100 - 1) {
+    while (token && argc < 1100 - 1) {
         argv[argc] = token;
         (argc)++;
         token = strtok(NULL, " ");
     }
-    argv[argc]=NULL;
+    argv[argc] = NULL;
     return argc;
 }
 
-
-int main (){
+int main() {
     char input[20050];
-  
     char *argv[1100];
-		while (1) {
-        
+
+    while (1) {
         printf("Femto shell prompt >");
-			  fflush(stdout);
-        
+        fflush(stdout);
+
         if (!fgets(input, sizeof(input), stdin)) break;
-        
-        input[strlen(input) - 1] = '\0';        // 0  == NULL
-        
+
+        input[strlen(input) - 1] = '\0';  // remove newline
+
         if (input[0] == '\0') continue;
 
-        int argc= tokenize(input, argv);
-        
+        int argc = tokenize(input, argv);
+
         if (argc == 0) continue; // empty command, skip
-        
-        if (!strcmp(argv[0], "exit")) {     // like ^z or ^c
+
+        if (!strcmp(argv[0], "exit")) {
             printf("Good Bye\n");
             fflush(stdout);
             exit(0);
+        } else if (!strcmp(argv[0], "echo")) {
+            my_echo(argc, argv);
+            fflush(stdout);
+            continue;
+        } else {
+            printf("Invalid command\n");
+            printf("Femto shell prompt >");
+            fflush(stdout);
+            return 1;
         }
-        else if(!strcmp(argv[0], "echo")){
-	        my_echo(argc,argv);
-	        fflush(stdout);
-	        continue;
-        }
-        else{
-             printf("Invalid command\n");
-             printf("Femto shell prompt >");
-             fflush(stdout);
-             return 1;
-        }
-        
     }
-return 0;
+    return 0;
 }
